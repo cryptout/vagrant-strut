d9f4efcfc589a82c2c8d2a03ee93f6657a487354
cryptout <cryptout@corputty.net>
2015-05-30 14:13:02 +0200

    initial clean upload

---
+++ provision.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# provision strut
+
+apt-get autoremove
+apt-get install -y npm
+apt-get install -y nodejs-legacy
+apt-get install -y git
+cd /store
+git clone git://github.com/tantaman/Strut.git
+cd Strut
+npm install -g grunt-cli
+npm install
+grunt build
+grunt server &
